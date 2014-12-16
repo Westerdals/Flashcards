d35db75eaf3f985920bb9e6f5c2a6b1507167944
Martin Lehmann <lehmar11@student.westerdals.no>
2014-12-16 23:28:53 +0100

    Rename hurrdurr.html to manage_flashcards.html

--- flashcards-node/client/js/app.js
+++ flashcards-node/client/js/app.js
@@ -7,7 +7,7 @@ flashcardsApp.config(['$routeProvider',
     function ($routeProvider) {
         $routeProvider
             .when('/flashcards', {
-                templateUrl: 'partials/hurrdurr.html',
+                templateUrl: 'partials/manage_flashcards.html',
                 controller: 'CardsAdminController'
             })
             .otherwise({
